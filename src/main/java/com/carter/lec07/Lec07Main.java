ad0d796b71aeab81e7337b58385a061c5c7af773
chunsang.yu <chunsang.yu@gmail.com>
2022-10-11 21:03:40 +0900

    feat: add Lec07 examples

--- src/main/java/com/carter/lec07/Lec07Main.java
+++ src/main/java/com/carter/lec07/Lec07Main.java
@@ -1,10 +1,17 @@
 package com.carter.lec07;
 
+import java.io.File;
 import java.io.IOException;
 
 public class Lec07Main {
 
   public static void main(String[] args) throws IOException {
+    JavaFilePrinter printer = new JavaFilePrinter();
+    printer.readFile(getFilePath("/a.txt"));
   }
 
+  private static String getFilePath(String fileName) {
+    File currentFile = new File(".");
+    return String.format("%s/%s", currentFile.getAbsolutePath(), fileName);
+  }
 }
