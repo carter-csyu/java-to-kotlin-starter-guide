c1fcddb201261e998c57740b55551936d6672fa3
chunsang.yu <chunsang.yu@gmail.com>
2022-10-31 18:39:00 +0900

    feat: add Lec19 examples

---
+++ src/main/kotlin/com/carter/lec19/UltraSuperGuardianTribe.kt
@@ -0,0 +1,7 @@
+package com.carter.lec19
+
+data class UltraSuperGuardianTribe(
+    val name: String
+)
+
+typealias USGTMap = Map<String, UltraSuperGuardianTribe>
---
+++ src/main/kotlin/com/carter/lec19/b/Print.kt
@@ -0,0 +1,5 @@
+package com.carter.lec19.b
+
+fun printHelloWorld() {
+    println("Hello world B")
+}
